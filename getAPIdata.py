77ec9662658d2e088fab202c4b7a7aa9eb17208f
Sean Yeo <seansqyeo@gmail.com>
2019-08-15 20:09:09 -0700

    added console input and smtp usage

---
+++ getAPIdata.py
@@ -0,0 +1,35 @@
+import requests
+#import config
+import sys
+
+from config import Config
+
+# def formatResponse(forexData):
+#     try:
+#         base =
+
+def get_currency_data(baseAndEx):#if custom input, have baseAndEx in function parameter
+    forex_api_key = "5c0bfe98cbbc7f78ad05"
+    url = "https://free.currconv.com/api/v7/convert?compact=ultra&apiKey=5c0bfe98cbbc7f78ad05"
+    formattedForexKey = baseAndEx["base"] + "_" + baseAndEx["exchange"]
+    # formattedForexKey = "USD_CAD"
+    parameters = {"APPID": forex_api_key, 'q': formattedForexKey}
+    response = requests.get(url, params=parameters)
+    # print(response.json())
+    return (response.json(), formattedForexKey)
+
+def get_base_currency():
+    base = input("base currency: ")
+    return base
+
+def get_exchange_currency():
+    exchange = input("exchange currency: ")
+    return exchange
+
+def baseAndEx():
+    baseAndEx = {"base": get_base_currency(), "exchange": get_exchange_currency()}
+    return baseAndEx
+
+#get_currency_data()
+
+
---
+++ notifier.py
@@ -0,0 +1,43 @@
+import smtplib
+from getAPIdata import *#get all methods
+#import script
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+email = "fortestingforex@gmail.com"
+password = "a0b1c2d3"
+send_to_email = "fortestingforex1@gmail.com"
+
+subject = "desired rate detected"
+message = ""
+
+msg = MIMEMultipart()
+msg["From"] = email
+msg["To"] = send_to_email
+msg["Subject"] = subject
+
+# forex_data = get_currency_data()
+# print(forex_data["USD_CAD"])
+# message += "USD is: " + str(forex_data["USD_CAD"]) + " CAD dollars"
+# print(message)
+
+baseAndEx = baseAndEx()
+base = baseAndEx["base"]
+exchange = baseAndEx["exchange"]
+
+try:
+    (forex_data, formattedKey) = get_currency_data(baseAndEx)
+    message += base + " is: " + str(forex_data[formattedKey]) + " " + exchange + " dollars"
+
+    msg.attach(MIMEText(message, "plain"))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(email, password)
+    text = msg.as_string()
+    server.sendmail(email, send_to_email, text)
+    server.quit()
+except:
+    print("problem retrieving data")
+
+
